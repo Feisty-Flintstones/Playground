ffe196a7d8e00c438d8c96763697413525cdac7b
Robert Schnek <robert.schnek@gmail.com>
2019-10-06 15:30:17 -0400

    created a seed file that saves our boards and items to the db. Also, through table has the correct columns

--- js/server/models/item.js
+++ js/server/models/item.js
@@ -9,19 +9,29 @@ const Item = db.define('item', {
             notEmpty: true
         }
     },
-    imageUrl: {
+    source: {
         type: Sequelize.STRING,
         allowNull: false,
         validate: {
             notEmpty: true
         }
+    },
+    resources: {
+        type: Sequelize.STRING,
+    },
+    is3D: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
     }
 })
 
 Item.parseData = function(body) { //possibly useful to change Viroreact stuff into db stuff
     const dbinstances = {
         name: body.name,
-        imageUrl: body.imageUrl
+        imageUrl: body.imageUrl,
+        source: body.source,
+        resources: body.resources,
+        is3D: body.is3D
     }
     return dbinstances
 }
--- js/server/models/objective.js
+++ js/server/models/objective.js
@@ -1,31 +1,16 @@
-const Sequelize = require('sequelize')
-const db = require('../db')
+// const Sequelize = require('sequelize')
+// const db = require('../db')
 
-const Objective = db.define('objective', {
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true
-        }
-    },
-    imageUrl: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true
-        }
-    }
-})
+// const Objective = db.define('objective', {
+//     name: {
+//         type: Sequelize.STRING,
+//         allowNull: false,
+//         validate: {
+//             notEmpty: true
+//         }
+//     }
+// })
 
-Objective.parseData = function(body) { //possibly useful to change Viroreact stuff into db stuff
-    const dbinstances = {
-        name: body.name,
-        type: body.type,
-        coordinates: body.coordinates,
-        imageUrl: body.imageUrl
-    }
-    return dbinstances
-}
 
-module.exports = Objective
+
+// module.exports = Objective

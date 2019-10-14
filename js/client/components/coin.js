272cfd24795e1fabd3779132efbe6fdafbd9719e
John Vitales <JohnieeJuiceboxx@gmail.com>
2019-10-14 17:22:00 -0400

    coin sfx

--- js/client/components/coin.js
+++ js/client/components/coin.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Viro3DObject, ViroAnimations } from 'react-viro';
+import { Viro3DObject, ViroAnimations, ViroSound } from 'react-viro';
 import { View } from 'react-native';
 import { connect } from 'react-redux';
 import { removeFromBoard } from '../store/boardReducer';
@@ -8,10 +8,12 @@ class Coin extends React.Component {
   constructor() {
     super();
     this.state = {
-      visible: true
+      visible: true,
+      coinSound: false
     };
   }
   render() {
+    console.log(this.state);
     ViroAnimations.registerAnimations({
       rotate: {
         properties: {
@@ -24,17 +26,28 @@ class Coin extends React.Component {
       <View>
         <Viro3DObject
           source={require('../res/animated_objects/coin/coin.vrx')}
-          type='VRX'
+          type="VRX"
           position={[this.props.xpos, this.props.ypos, this.props.zpos]}
           scale={[0.0005, 0.0005, 0.0005]}
           onClick={() => {
             this.props.addCoins();
+            console.log(this.state);
             this.setState({
-              visible: false
+              visible: false,
+              coinSound: true
             });
           }}
           animation={{ name: 'rotate', run: true, loop: true }}
           visible={this.state.visible && this.props.visible}
+        />
+        <ViroSound
+          paused={!this.state.coinSound}
+          // muted={this.state.coinSound}
+          source={require('../../../assets/ring_sound_effect.mp3')}
+          loop={false}
+          volume={1.0}
+
+          // onError={this.onErrorSound}
         />
       </View>
     );

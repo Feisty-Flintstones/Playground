f6f1a7698e020b128eb2b204d535f5697c72e3a2
John Vitales <JohnieeJuiceboxx@gmail.com>
2019-10-15 12:58:18 -0400

    added spotlight in tutorial

--- js/client/components/coin.js
+++ js/client/components/coin.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { Viro3DObject, ViroAnimations, ViroSound } from 'react-viro';
+import {
+  Viro3DObject,
+  ViroAnimations,
+  ViroSound,
+  ViroSpotLight
+} from 'react-viro';
 import { View } from 'react-native';
 import { connect } from 'react-redux';
 import { removeFromBoard } from '../store/boardReducer';
@@ -23,6 +28,26 @@ class Coin extends React.Component {
     });
     return (
       <View>
+        <ViroSpotLight
+          innerAngle={5}
+          outerAngle={25}
+          direction={[0, -1, 0]}
+          position={[this.props.xpos, this.props.ypos + 3, this.props.zpos]}
+          color="#e9e9e9"
+          castsShadow={true}
+          shadowMapSize={2048}
+          shadowNearZ={2}
+          shadowFarZ={7}
+          shadowOpacity={0.7}
+        />
+        <ViroSpotLight
+          innerAngle={5}
+          outerAngle={90}
+          direction={[0, -1, -0.2]}
+          position={[this.props.xpos, this.props.ypos + 3, this.props.zpos + 1]}
+          color="#ffffff"
+          castsShadow={true}
+        />
         <Viro3DObject
           source={require('../res/animated_objects/coin/coin.vrx')}
           type="VRX"
